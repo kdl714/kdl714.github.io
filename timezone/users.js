879686d5064e9f7de4fe0827768f606ab7abadf1
kdl714 <69640403+kdl714@users.noreply.github.com>
2024-06-04 12:03:25 -0400

    Create users.js

---
+++ timezone/users.js
@@ -0,0 +1,90 @@
+let users = [
+    {
+        "user_name": "Kevin",
+        "utc_offset": -4,
+        "start_time": 9,
+        "end_time": 17.5,
+        "tentative_times": [
+            {
+                "start_time":8,
+                "end_time":9
+            },
+            {
+                "start_time":20,
+                "end_time":23.5
+            }
+        ]
+    },
+    {
+        "user_name": "Lea",
+        "utc_offset": 8,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Rowi",
+        "utc_offset": 8,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Miro",
+        "utc_offset": 2,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Ramkumar",
+        "utc_offset": 2,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Charles",
+        "utc_offset": 2,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Sarah",
+        "utc_offset": 8,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "Jojo",
+        "utc_offset": 8,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    },
+    {
+        "user_name": "IST",
+        "utc_offset": 5.5,
+        "start_time": 9,
+        "end_time": 17,
+        "tentative_times": [
+            
+        ]
+    }
+]
